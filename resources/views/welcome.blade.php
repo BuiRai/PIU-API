0b6c2564c98f0dba9706f71791419756a2415714
buirai <buirai_naetle@hotmail.com>
2017-01-30 21:40:33 -0600

    Init to design main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
+        {!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
         <style>
             html, body {
                 background-color: #fff;
@@ -65,6 +66,20 @@
         </style>
     </head>
     <body>
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </nav>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
